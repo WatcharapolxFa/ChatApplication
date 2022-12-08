6662eb58eab26de21c5710a519096e4d7c31be9b
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-12-09 01:28:53 +0700

    Source

---
+++ src/pages/Register.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Register = () => {
+    <div className="fromContaniner">
+        <div className="fromWrapper">
+            <span className="logo"> Chat </span>
+            <span className="title"> Register </span>
+        <form>
+            <input type="text" placeholder="display name"/>
+            <input type="email" placeholder="email"/>
+            <input type="password" placeholder="password"/>
+            <input type="file" />
+            <button> Singn up </button>
+        </form>
+        <p> Tou have an account ? Login</p>
+        </div>
+    </div>
+}
+
+export default Register;
